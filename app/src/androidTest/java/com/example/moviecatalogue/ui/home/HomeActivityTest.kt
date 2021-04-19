4d226379ef59e3b0a61f876fbe224180c7833150
DesiMusfiroh <musfiroh.desi@gmail.com>
2021-04-19 21:14:29 +0700

    instrument testing

--- app/src/androidTest/java/com/example/moviecatalogue/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/example/moviecatalogue/ui/home/HomeActivityTest.kt
@@ -2,7 +2,8 @@ package com.example.moviecatalogue.ui.home
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -18,6 +19,18 @@ class HomeActivityTest {
 
     @get:Rule
     var activityRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Test
+    fun swipePage() {
+        onView(withId(R.id.view_pager)).check(matches(isDisplayed()))
+        onView(withId(R.id.view_pager)).perform(swipeLeft())
+        onView(withId(R.id.view_pager)).perform(swipeRight())
+    }
+
+    @Test
+    fun checkTabLayoutDisplayed() {
+        onView(withId(R.id.tabs)).perform(click()).check(matches(isDisplayed()))
+    }
 
     @Test
     fun loadMovies() {
@@ -38,6 +51,9 @@ class HomeActivityTest {
         onView(withId(R.id.tv_rating)).check(matches(withText("${dummyMovies[0].rating}/10" )))
         onView(withId(R.id.tv_popularity)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_popularity)).check(matches(withText(dummyMovies[0].popularity.toString())))
+        onView(withId(R.id.img_poster)).check(matches(isDisplayed()))
+        onView(withId(R.id.img_backdrop)).check(matches(isDisplayed()))
+        pressBack()
     }
 
     @Test
@@ -46,4 +62,25 @@ class HomeActivityTest {
         onView(withId(R.id.rv_tvshow)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShow.size))
     }
+
+    @Test
+    fun loadDetailTvShow() {
+        onView(withText("Tv Shows")).perform(click())
+        onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
+        onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, click()))
+        onView(withId(R.id.tv_name)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_name)).check(matches(withText(dummyTvShow[10].name)))
+        onView(withId(R.id.tv_desc)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_desc)).check(matches(withText(dummyTvShow[10].desc)))
+        onView(withId(R.id.tv_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_date)).check(matches(withText(dummyTvShow[10].date)))
+        onView(withId(R.id.tv_rating)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_rating)).check(matches(withText("${dummyTvShow[10].rating}/10" )))
+        onView(withId(R.id.tv_popularity)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_popularity)).check(matches(withText(dummyTvShow[10].popularity.toString())))
+        onView(withId(R.id.img_poster)).check(matches(isDisplayed()))
+        onView(withId(R.id.img_backdrop)).check(matches(isDisplayed()))
+        pressBack()
+    }
+
 }
--- app/src/test/java/com/example/moviecatalogue/ui/home/tvshow/TvShowViewModelTest.kt
+++ app/src/test/java/com/example/moviecatalogue/ui/home/tvshow/TvShowViewModelTest.kt
@@ -1,7 +1,6 @@
 package com.example.moviecatalogue.ui.home.tvshow
 
 import org.junit.Test
-
 import org.junit.Assert.*
 import org.junit.Before
 
