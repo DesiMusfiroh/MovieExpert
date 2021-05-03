ad4423f09ba2bc5bc79cb695869d78ccc17065e3
DesiMusfiroh <musfiroh.desi@gmail.com>
2021-05-03 21:17:00 +0700

    instrument test

--- app/src/androidTest/java/com/example/moviecatalogue/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/example/moviecatalogue/ui/home/HomeActivityTest.kt
@@ -7,18 +7,16 @@ import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.moviecatalogue.R
-import com.example.moviecatalogue.utils.DataDummy
 import com.example.moviecatalogue.utils.EspressoIdlingResource
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 class HomeActivityTest {
-    private val dummyMovies = DataDummy.generateDummyMovies()
-    private val dummyTvShow = DataDummy.generateDummyTvShows()
 
     @Before
     fun setUp() {
@@ -46,51 +44,27 @@ class HomeActivityTest {
     @Test
     fun loadMovies() {
         onView(withId(R.id.rv_movie)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovies.size))
-    }
-
-    @Test
-    fun loadDetailMovie() {
-        onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        onView(withId(R.id.tv_name)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_name)).check(matches(withText(dummyMovies[0].name)))
-        onView(withId(R.id.tv_desc)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_desc)).check(matches(withText(dummyMovies[0].desc)))
-        onView(withId(R.id.tv_date)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_date)).check(matches(withText(dummyMovies[0].date)))
-        onView(withId(R.id.tv_rating)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_rating)).check(matches(withText("${dummyMovies[0].rating}/10" )))
-        onView(withId(R.id.tv_popularity)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_popularity)).check(matches(withText(dummyMovies[0].popularity.toString())))
-        onView(withId(R.id.img_poster)).check(matches(isDisplayed()))
-        onView(withId(R.id.img_backdrop)).check(matches(isDisplayed()))
-        pressBack()
+        onView(withId(R.id.rv_movie)).perform(scrollToPosition<RecyclerView.ViewHolder>(10))
     }
 
     @Test
     fun loadTvShows() {
         onView(withText("Tv Shows")).perform(click())
         onView(withId(R.id.rv_tvshow)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShow.size))
+        onView(withId(R.id.rv_tvshow)).perform(scrollToPosition<RecyclerView.ViewHolder>(10))
     }
 
     @Test
-    fun loadDetailTvShow() {
-        onView(withText("Tv Shows")).perform(click())
-        onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(2))
-        onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
+    fun loadDetailMovie() {
+        onView(withId(R.id.rv_movie)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(5, click()))
         onView(withId(R.id.tv_name)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_name)).check(matches(withText(dummyTvShow[2].name)))
         onView(withId(R.id.tv_desc)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_desc)).check(matches(withText(dummyTvShow[2].desc)))
         onView(withId(R.id.tv_date)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_date)).check(matches(withText(dummyTvShow[2].date)))
         onView(withId(R.id.tv_rating)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_rating)).check(matches(withText("${dummyTvShow[2].rating}/10" )))
         onView(withId(R.id.tv_popularity)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_popularity)).check(matches(withText(dummyTvShow[2].popularity.toString())))
         onView(withId(R.id.img_poster)).check(matches(isDisplayed()))
         onView(withId(R.id.img_backdrop)).check(matches(isDisplayed()))
         pressBack()
     }
+
 }
