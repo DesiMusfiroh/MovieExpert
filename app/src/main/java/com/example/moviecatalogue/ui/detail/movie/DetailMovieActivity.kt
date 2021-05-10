dcff114bb72a2e6ab25b293c2e2e9146d8e9b4c8
DesiMusfiroh <musfiroh.desi@gmail.com>
2021-05-10 16:49:40 +0700

    set favorite movie

--- app/src/main/java/com/example/moviecatalogue/ui/detail/movie/DetailMovieActivity.kt
+++ app/src/main/java/com/example/moviecatalogue/ui/detail/movie/DetailMovieActivity.kt
@@ -3,17 +3,18 @@ package com.example.moviecatalogue.ui.detail.movie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.example.moviecatalogue.R
-import com.example.moviecatalogue.data.model.Movie
 import com.example.moviecatalogue.data.source.local.entity.MovieEntity
 import com.example.moviecatalogue.databinding.ActivityDetailMovieBinding
-import com.example.moviecatalogue.ui.home.HomeActivity
 import com.example.moviecatalogue.utils.Constants.BACKDROP_URL
 import com.example.moviecatalogue.utils.Constants.POSTER_URL
 import com.example.moviecatalogue.viewmodel.ViewModelFactory
@@ -24,18 +25,20 @@ class DetailMovieActivity : AppCompatActivity(), View.OnClickListener{
         const val EXTRA_MOVIE = "extra_movie"
     }
     private lateinit var binding: ActivityDetailMovieBinding
+    private lateinit var viewModel: DetailMovieViewModel
+    private var menu: Menu? = null
     private var movieId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar?.hide()
-        binding.btnBack.setOnClickListener(this)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         binding.btnShare.setOnClickListener(this)
 
         val factory = ViewModelFactory.getInstance(this)
-        val viewModel = ViewModelProvider(this, factory)[DetailMovieViewModel::class.java]
+        viewModel = ViewModelProvider(this, factory)[DetailMovieViewModel::class.java]
 
         val extras = intent.extras
         if (extras != null) {
@@ -81,7 +84,6 @@ class DetailMovieActivity : AppCompatActivity(), View.OnClickListener{
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.btn_back -> { startActivity(Intent(this, HomeActivity::class.java)) }
             R.id.btn_share -> {
                 val shareIntent = Intent()
                 shareIntent.action = Intent.ACTION_SEND
@@ -89,6 +91,43 @@ class DetailMovieActivity : AppCompatActivity(), View.OnClickListener{
                 shareIntent.putExtra(Intent.EXTRA_TEXT,  getString(R.string.url_movie, movieId))
                 startActivity(Intent.createChooser(shareIntent, getString(R.string.send)))
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        this.menu = menu
+        viewModel.getMovie.observe(this, { movie ->
+            if (movie != null) {
+                when (movie.status) {
+                    Status.LOADING -> Toast.makeText(applicationContext, "Loading", Toast.LENGTH_SHORT).show()
+                    Status.SUCCESS -> if (movie.data != null) {
+                        populateMovie(movie.data)
+                        val state = movie.data.favorited
+                        setFavoriteState(state)
+                    }
+                    Status.ERROR -> Toast.makeText(applicationContext, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_favorite) {
+            viewModel.setFavorite()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setFavoriteState(state: Boolean) {
+        if (menu == null) return
+        val menuItem = menu?.findItem(R.id.action_favorite)
+        if (state) {
+            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_favorite_on)
+        } else {
+            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_favorite_off)
         }
     }
 }
